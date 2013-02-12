4f1206131aef7d4622a158692e49029c2b2a4c45
Andrew Tran <atran246@gmail.com>
2013-02-11 20:33:07 -0600

    Ex 140 Data Defininitions

---
+++ ex 140.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 140|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;Andrew Tran ex 140
+
+; A List-of-Ftemperatures is one of:
+; - empty
+; - (cons FTemperature List-of-temperatures)
+; interp.: a list of Fahrenheit temperatures
+
+; A List-of-Ctemperatures is one of:
+; - empty
+; - (cons Temperature List-of-temperatures)
+; interp.: a list of Celsius temperatures
+
+
