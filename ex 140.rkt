44a19ba0c1de03ff45f616601e394c67c25316c7
Andrew Tran <atran246@gmail.com>
2013-02-11 20:45:05 -0600

    Ex 140 Template

--- ex 140.rkt
+++ ex 140.rkt
@@ -17,4 +17,7 @@
 ; List-of-Ftemperatures -> List-of-Ctemperatures
 ; convert a list of fahrenheit temperatures to a list of celsius temperatures
 (check-expect (convertFC (list 5 41)) (list -15 5))
-(define (convertFC) empty)
+(define (convertFC lot) 
+  (cond
+    [(empty? (rest lot)) ... ]
+    [(cons? lot) ...  (first lot)... (convertFC (rest lot))...]))
